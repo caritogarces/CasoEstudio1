26841179653ff8295f1b953e730f46e73e9650bd
carogarces <stevenrojas@stevenrojas-PC>
2012-08-31 10:31:27 -0500

    Punto 4 Implementacion

--- src/co/edu/eafit/carritocompras/data/Customer.java
+++ src/co/edu/eafit/carritocompras/data/Customer.java
@@ -1,5 +1,6 @@
 package co.edu.eafit.carritocompras.data;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 public class Customer {
@@ -7,11 +8,32 @@ public class Customer {
 	private String code;
 	private String name;
 	private List<Purchase> purchases;
+	//new variable for points logic
+	private int userPoints;
+
+	public int getUserPoint() {
+		return userPoints;
+	}
+
+	public void setUserPoint(int userPoint) {
+		this.userPoints = userPoint;
+	}
 
 	public Customer(String code, String name) {
 		super();
 		this.code = code;
 		this.name = name;
+		this.userPoints = 0;
+	}
+	
+	public int computePoint(BigDecimal totalPrice){
+		int umbralSumaPuntos = 1000;//por cada 1000 se suma un punto 
+		BigDecimal umbralSumaPuntosBD = new BigDecimal(umbralSumaPuntos),receivedPoints = new BigDecimal(0);
+	//se obtiene el numero de puntos recibidos
+		receivedPoints = totalPrice.divide(umbralSumaPuntosBD);
+
+		userPoints = receivedPoints.intValueExact();
+		return receivedPoints.intValueExact();
 	}
 
 	public String getCode() {
--- src/co/edu/eafit/carritocompras/service/PaymentService.java
+++ src/co/edu/eafit/carritocompras/service/PaymentService.java
@@ -1,5 +1,7 @@
 package co.edu.eafit.carritocompras.service;
 
+import java.math.BigDecimal;
+
 import co.edu.eafit.carritocompras.data.Customer;
 import co.edu.eafit.carritocompras.data.Purchase;
 import co.edu.eafit.carritocompras.data.PurchaseStatus;
@@ -15,6 +17,12 @@ public class PaymentService {
 				purchase.setStatus(PurchaseStatus.APPROVED);
 			} else {
 				purchase.setStatus(PurchaseStatus.REJECTED);
+			}
+			if (customer.getUserPoint() >= 1000) {//se determina que  debe aplicarse descuento porque son 1000 puntos o mas
+				BigDecimal bd = new BigDecimal(0.98);//el descuento es del 2%, entonces el nuevo valor seria del 98% de lo que era antes
+				bd.multiply(purchase.getTotalPrice());//Se calcula el nuevo valor con el descuento
+				purchase.setTotalPrice(bd);//se establece el resultado como un nuevo total
+
 			}
 		} catch (ChangeStatusException e) {
 			purchase.markAsReview();
